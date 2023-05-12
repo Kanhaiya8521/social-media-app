6833038bf11143fa6150accf79111eafcbfb77ee
Kanhaiya kumar <kanhaiya15399@gmail.com>
2023-05-13 02:16:27 +0530

    added auth partially

--- src/index.js
+++ src/index.js
@@ -4,16 +4,18 @@ import {ToastProvider} from 'react-toast-notifications';
 import './styles/index.css';
 import {App} from './components/index';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthProvider } from './providers/AuthProvider';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
       <Router>
-        <App />
+        <AuthProvider>
+          <App />
+        </AuthProvider>
       </Router>
     </ToastProvider>
   </React.StrictMode>
 );
-
 
