b0e052dad7d2d9c62b2ee4d9fe98525ce4773775
Kanhaiya kumar <kanhaiya15399@gmail.com>
2023-05-12 23:12:31 +0530

    added login page with proper notification

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {ToastProvider} from 'react-toast-notifications';
 import './styles/index.css';
 import {App} from './components/index';
 import { BrowserRouter as Router } from 'react-router-dom';
@@ -7,9 +8,11 @@ import { BrowserRouter as Router } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <ToastProvider autoDismiss autoDismissTimeout={5000} placement="top-left">
+      <Router>
+        <App />
+      </Router>
+    </ToastProvider>
   </React.StrictMode>
 );
 
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,21 +1,72 @@
 import styles from '../styles/login.module.css';
+import { useState } from 'react';
+import { useToasts } from 'react-toast-notifications';
+import {login} from '../api';
 
 const Login = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [loggingIn, setLoggingIn] = useState(false);
+    const { addToast } = useToasts();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        setLoggingIn(true);
+
+        if(!email || !password) {
+            return addToast(
+              'Please enter both email and password',
+              {
+                appearance: 'error',
+                // autoDismiss: true,
+              },
+            );
+        }
+        const response = await login(email, password);
+
+        if(response.success) {
+            addToast('Successfully logged in', {
+                appearance: 'success',
+            });
+        }
+        else {
+            addToast(response.message, {
+              appearance: 'error',
+            });
+
+        }
+        setLoggingIn(false);
+        console.log('loggingIn', loggingIn);
+    };
+
+
     return (
-      <form className={styles.loginForm}>
+      <form className={styles.loginForm} onSubmit={handleSubmit}>
         <spam className={styles.loginSignupHeader}>Log In</spam>
 
         <div className={styles.field}>
-          <input type="email" placeholder="Email" required />
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <div className={styles.field}>
-          <input type="password" placeholder="Password" required />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
         </div>
         <div className={styles.field}>
-          <button>Log In</button>
+          <button disabled={loggingIn}>
+            {loggingIn ? 'Logging in...' : 'Log In'}
+          </button>
         </div>
       </form>
     );
-};
+};   
 
 export default Login;
