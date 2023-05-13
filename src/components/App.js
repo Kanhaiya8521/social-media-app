43b4936f44edb4ac4634e54a93c1f6eae8067d10
Kanhaiya kumar <kanhaiya15399@gmail.com>
2023-05-14 01:15:52 +0530

    useAuth in Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,38 +1,30 @@
-import { useEffect, useState } from "react";
-import {getPosts} from '../api'
 import { Home, Login } from './../pages/index'
 import {Loader, Navbar} from './';
 import { Routes, Route } from "react-router-dom";
 import { useAuth } from "../hooks";
 
-const Signup = () => {
-  return <h1>signup page</h1>;
-};
-const Register = () => {
-  return <h1>register page</h1>;
-};
 const Page404 = () => {
   return <h1>404</h1>;
 };
 
 function App() {
-  const [posts, setPosts] = useState([]);
+  // const [posts, setPosts] = useState([]);
   // loading islia hataya ki auth me loading hai
   // const [loading, setLoading] = useState(true);
   const auth = useAuth();
 
-  useEffect(() => {
-    const fetchPosts = async () => {
-      const response = await getPosts();
-      console.log('response', response);
-      if(response.success) {
-        setPosts(response.data.posts);
-      }
-      // setLoading(false);
-    };
+  // useEffect(() => {
+  //   const fetchPosts = async () => {
+  //     const response = await getPosts();
+  //     console.log('response', response);
+  //     if(response.success) {
+  //       setPosts(response.data.posts);
+  //     }
+  //     // setLoading(false);
+  //   };
     
-    fetchPosts();
-  }, []);
+  //   fetchPosts();
+  // }, []);
 
   if(auth.loading) {
     return <Loader />;
@@ -42,10 +34,8 @@ function App() {
     <div className="App">
       <Navbar />
       <Routes>
-        <Route exact path="/" element={<Home posts={posts} />} />
+        <Route exact path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/register" element={<Register />} />
         <Route path="*" element={<Page404 />} />
       </Routes>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,31 @@
-import PropTypes from 'prop-types'
+import { useEffect, useState } from 'react'
+import {getPosts} from '../api'
 import styles from '../styles/home.module.css';
-import Comment from './../components/Comment'; 
+import {Comment, Loader} from './../components'; 
 // This is not error maine ek baar comment js se save kiya tha file ko then Comment.js kar diya that's why issa message aa rha hai
 
-const Home = ({ posts }) => {
-  // console.log('posts', posts);
-  // posts.map( (post) => (console.log('post', post.user.name)))
+const Home = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await getPosts();
+
+      if (response.success) {
+        setPosts(response.data.posts);
+      }
+
+      setLoading(false);
+    };
+
+    fetchPosts();
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+  
   return (
     <div className={styles.postsList}>
       {posts.map((post) => (
@@ -37,29 +57,23 @@ const Home = ({ posts }) => {
                   src="https://cdn-icons-png.flaticon.com/128/1380/1380338.png"
                   alt="comments-icon"
                 />
-                <span>2</span>
+                <span>{post.comments.length}</span>
               </div>
             </div>
             <div className={styles.postCommentBox}>
               <input placeholder="Start typing a comment" />
             </div>
 
-            <div className={styles.postCommentsList}>
               <div className={styles.postCommentsList}>
                 {post.comments.map((comment) => (
                   <Comment comment={comment} key={`comment-${comment._id}`} />
                 ))}
-              </div>
             </div>
           </div>
         </div>
       ))}
     </div>
   );
-};
-
-Home.propTypes = {
-  posts: PropTypes.array.isRequired,
 };
 
 export default Home;
